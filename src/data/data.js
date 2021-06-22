227db86d2d2999a6e1f2b2f544a9a43b46767fb2
sunjus <sunju.shin1108@gmail.com>
2021-06-22 18:02:18 +0300

    Initial commit

---
+++ src/data/data.js
@@ -0,0 +1,44 @@
+export const projects = [
+  {
+    title: '',
+    description: "",
+      image: '/images/1.png',
+      tags: [],
+    source: '',
+    visit: '',
+    id: 0,
+  },
+  {
+    title: '',
+    description:"",
+    image: '/images/2.png',
+    tags: ['React'],
+    source: '',
+    visit: '',
+    id: 1,
+  },
+  {
+    title: '',
+    description: "",
+      image: '/images/3.jpg',
+      tags: ['React'],
+    source: '',
+    visit: '',
+    id: 2,
+  },
+  {
+    title: '',
+    description: "",
+    image: '/images/4.jpg',
+    tags: ['React'],
+    source: '',
+    visit: '',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2020, text: 'Started my journey', }, 
+  { year: 2021, text: 'Shared my projects with the world', },
+  
+];
