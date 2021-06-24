cc756242a4fca25d70db63d59c58aed5db73d1f3
sunjus <sunju.shin1108@gmail.com>
2021-06-25 01:25:28 +0300

    Chnaged color, layout

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
   <Section>
     <LeftSection>
       <SectionTitle>
-        Welcome to <br />
-        My Personal Portfolio.
+        Welcome to <br />A creative, adventurous, and curious Front-End
+        Developer's Portfolio.
       </SectionTitle>
       <SectionText>
-        I am Sunju, a Front-end Web Developer specialized in React. <br />
-        I love building clean and intuitive interfaces, conversations about design and delivering features
-        that help simplify a user's experience.
+        I am Sunju, a Front-end Web Developer specialized in React. <br />I love
+        building clean and intuitive interfaces, conversations about design and
+        delivering features that help simplify a user's experience.
       </SectionText>
-      <Button onClick={() => window.location = '#about'}>
-        Learn More
-      </Button>
+      <Button onClick={() => (window.location = "#about")}>Learn More</Button>
     </LeftSection>
   </Section>
 );
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,58 +1,76 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend, DiSass, DiNodejs } from 'react-icons/di';
-import { SiTypescript, SiJest, SiMongodb } from 'react-icons/si';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import { DiFirebase, DiReact, DiZend, DiSass, DiNodejs } from "react-icons/di";
+import { SiTypescript, SiJest, SiMongodb } from "react-icons/si";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+  ListIcon,
+} from "./TechnologiesStyles";
 
-const Technologies = () =>  (
+const Technologies = () => (
   <Section id="tech">
-  <SectionDivider divider />
-  <SectionTitle>Technologies</SectionTitle>
-  <SectionText>
-  Through consistent daily practice, I’ve managed to develop a skill set that covers both ends of the spectrum, 
-  from the front-end to the backend. Currently, my stronger side is front-end development.
-  </SectionText>
-  <List>
-    <ListItem>
-      <picture>
-        <DiReact size="4rem" />
-        <DiSass size='3.5rem' />
-        <SiTypescript size='3rem' />
-        <SiJest size='3rem' />
-      </picture>
-      <ListContainer>
-        <ListTitle>Front-End</ListTitle>
-        <ListParagraph>
-        I build interactive and responsive user interface using modern web-technologies and tools such as React.js, Typescript and Sass.
-        </ListParagraph>
-      </ListContainer>
-    </ListItem>
-    <ListItem>
-      <picture>
-        <DiFirebase size="4rem" />
-        <DiNodejs size='4rem'/>
-        <SiMongodb size='4rem'/>
-      </picture>
-      <ListContainer>
-        <ListTitle>Back-End</ListTitle>
-        <ListParagraph>
-        I use javascript on the server-side by utilizing its run-time node.js. Also, I have experience in interacting with the NoSQL database MongoDB.
-        </ListParagraph>
-      </ListContainer>
-    </ListItem>
-    <ListItem>
-      <picture>
-        <DiZend size="3rem" />
-      </picture>
-      <ListContainer>
+    <SectionDivider divider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      Through consistent daily practice, I’ve managed to develop a skill set
+      that covers both ends of the spectrum, from the front-end to the backend.
+      Currently, my stronger side is front-end development.
+    </SectionText>
+    <List>
+      <ListItem>
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListIcon src="/images/frontend.png" />
+          <ListParagraph>
+            I build interactive and responsive user interface using modern
+            web-technologies and tools such as React.js, Typescript and Sass.
+          </ListParagraph>
+          <picture>
+            <DiReact size="4rem" />
+            <DiSass size="3.5rem" />
+            <SiTypescript size="3rem" />
+            <SiJest size="3rem" />
+          </picture>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListIcon src="/images/backend.png" />
+          <ListParagraph>
+            I use javascript on the server-side by utilizing its run-time
+            node.js. Also, I have experience in interacting with the NoSQL
+            database MongoDB.
+          </ListParagraph>
+          <picture>
+            <DiFirebase size="4rem" />
+            <DiNodejs size="4rem" />
+            <SiMongodb size="4rem" />
+          </picture>
+        </ListContainer>
+      </ListItem>
+      {/*<ListItem>
+        <picture>
+          <DiZend size="3rem" />
+        </picture>
+        <ListContainer>
         <ListTitle>UI/UX</ListTitle>
         <ListParagraph>
           I have experience with tools like Figma
         </ListParagraph>
       </ListContainer>
-    </ListItem>
-  </List>
-</Section>
+      </ListItem>*/}
+    </List>
+  </Section>
 );
 
 export default Technologies;
